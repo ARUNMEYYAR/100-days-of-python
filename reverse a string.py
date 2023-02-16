dad988064849e80af338b1d863a05b42c1db9573
Arun <arunmeyyar007@gmail.com>
2023-02-16 19:43:57 +0530

    day 25

--- reverse a string.py
+++ reverse a string.py
@@ -1,10 +1,6 @@
-
-def reverse_string(s):
-    if len(s) == 0:
-        return s
-    else:
-        return reverse_string(s[1:]) + s[0]
-
-original_string = "Hello World!"
-reversed_string = reverse_string(original_string)
-print(reversed_string)
+pal=input("enter the string:")
+num=pal[::-1]
+if num==pal:
+    print("it is palindrome")
+else:
+    print("it is not palindorme")    
